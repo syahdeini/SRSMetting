7b7d6fc3cade7bb1747464c0433916566c8c02e6
wilik16 <wilik16@yahoo.com>
2014-08-13 15:24:14 +0700

    tamabh int

--- app/src/main/java/srsmeeting/iconpln/net/id/srsmeeting/Login.java
+++ app/src/main/java/srsmeeting/iconpln/net/id/srsmeeting/Login.java
@@ -12,6 +12,7 @@ public class Login extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        int a = 15;
     }
 
 
